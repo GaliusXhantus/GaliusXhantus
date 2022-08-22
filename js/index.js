d633545c22b3efdf7ab1e3d6f35e3b0d3ee7bdcb
GaliusXhantus <Xhantusgalius@gmail.com>
2022-08-22 00:42:26 +0000

    Haaa

--- js/index.js
+++ js/index.js
@@ -23,15 +23,17 @@ async function RenderPokemon(pokemon){
 
 	const dataPokemon = await GetPokemon(pokemon);
 	const pokeCard = CreateElement('div', 'poke-card');
+	const pokeImagediv = CreateElement('div', 'poke-image-div');
 	const pokeImage = CreateElement('img', 'poke-image');
 	const pokeId = CreateElement('p', 'poke-id');
 	const pokeName = CreateElement('p', 'poke-name');
 
 	main.appendChild(pokeCard);
-	pokeCard.appendChild(pokeImage);
+	pokeCard.appendChild(pokeImagediv);
+	pokeImagediv.appendChild(pokeImage);
 	pokeCard.appendChild(pokeId);
 	pokeCard.appendChild(pokeName);
-
+	
 
     pokeImage.src = dataPokemon['sprites']['other']['official-artwork']['front_default'];
 	pokeId.innerHTML = dataPokemon.id;
